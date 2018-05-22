80b2c528f413fddf42fd9f4b25b9ae402bf514bf
zay7sev <filavtev@iosz.net>
2018-05-22 23:34:39 +0300

    server.lua
    transfers notification now displays names instead of numbers

--- server.lua
+++ server.lua
@@ -11,11 +11,11 @@ AddEventHandler('bank:deposit', function(amount)
 	
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
-		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+		TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Withdraw Money', 'Invalid amount', 'CHAR_BANK_MAZE', 9)
 	else
 		xPlayer.removeMoney(amount)
 		xPlayer.addAccountMoney('bank', tonumber(amount))
-	end
+		TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Deposit Money', 'Deposited ~g~$' .. amount .. '~s~', 'CHAR_BANK_MAZE', 9)	end
 end)
 
 
@@ -31,6 +31,7 @@ AddEventHandler('bank:withdraw', function(amount)
 	else
 		xPlayer.removeAccountMoney('bank', amount)
 		xPlayer.addMoney(amount)
+		TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Withdraw Money', 'Withdrawed ~r~$' .. amount .. '~s~', 'CHAR_BANK_MAZE', 9)
 	end
 end)
 
@@ -49,18 +50,24 @@ AddEventHandler('bank:transfer', function(to, amountt)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	local zPlayer = ESX.GetPlayerFromId(to)
+	-- names instead of numbers
+	local xName   = GetPlayerName(_source)
+	local zName   = GetPlayerName(to)
 	local balance = 0
 	balance = xPlayer.getAccount('bank').money
 	zbalance = zPlayer.getAccount('bank').money
+
 	
 	if tonumber(_source) == tonumber(to) then
-		TriggerClientEvent('chatMessage', _source, "You cannot transfer to your self")
+		TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Transfer Money', 'You cannot transfer to your self!', 'CHAR_BANK_MAZE', 9)
 	else
 		if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
-			TriggerClientEvent('chatMessage', _source, "You don't have enough money in the bank.")
+			TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Transfer Money', 'Not enough money to transfer!', 'CHAR_BANK_MAZE', 9)
 		else
 			xPlayer.removeAccountMoney('bank', amountt)
 			zPlayer.addAccountMoney('bank', amountt)
+			TriggerClientEvent('esx:showAdvancedNotification', _source, 'Bank', 'Transfer Money', 'You transfered ~r~$' .. amountt .. '~s~ to ~r~' .. zName .. ' .', 'CHAR_BANK_MAZE', 9)
+			TriggerClientEvent('esx:showAdvancedNotification', to, 'Bank', 'Transfer Money', 'You received ~r~$' .. amountt .. '~s~ from ~r~ the bank by '.. xName ..' .', 'CHAR_BANK_MAZE', 9)
 		end
 		
 	end
